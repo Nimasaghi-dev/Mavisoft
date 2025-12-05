5d92d567d27edb4b638bb5efd095a776015139a1
nima <nimasaghi.dev@gmail.com>
2025-12-05 17:00:33 +0100

    initial commit

---
+++ src/app/(app)/orders/page.tsx
@@ -0,0 +1,50 @@
+import { Avatar } from '@/components/avatar'
+import { Button } from '@/components/button'
+import { Heading } from '@/components/heading'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/table'
+import { getOrders } from '@/data'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Orders',
+}
+
+export default async function Orders() {
+  let orders = await getOrders()
+
+  return (
+    <>
+      <div className="flex items-end justify-between gap-4">
+        <Heading>Orders</Heading>
+        <Button className="-my-0.5">Create order</Button>
+      </div>
+      <Table className="mt-8 [--gutter:--spacing(6)] lg:[--gutter:--spacing(10)]">
+        <TableHead>
+          <TableRow>
+            <TableHeader className='text-amber-50'>Order number</TableHeader>
+            <TableHeader className='text-amber-100'>Purchase date</TableHeader>
+            <TableHeader className='text-amber-200'>Customer</TableHeader>
+            <TableHeader className='text-amber-300'>Event</TableHeader>
+            <TableHeader className="text-right text-amber-400">Amount</TableHeader>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {orders.map((order) => (
+            <TableRow key={order.id} href={order.url} title={`Order #${order.id}`}>
+              <TableCell>{order.id}</TableCell>
+              <TableCell className="text-zinc-500">{order.date}</TableCell>
+              <TableCell>{order.customer.name}</TableCell>
+              <TableCell>
+                <div className="flex items-center gap-2">
+                  <Avatar src={order.event.thumbUrl} className="size-6" />
+                  <span>{order.event.name}</span>
+                </div>
+              </TableCell>
+              <TableCell className="text-right">US{order.amount.usd}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </>
+  )
+}
