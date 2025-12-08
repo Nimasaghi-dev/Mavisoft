365884e5449486fbff96a97ca8aa92c2b2d4be87
nima <nimasaghi.dev@gmail.com>
2025-12-08 15:51:51 +0100

    fix: Remove the wrapper div around HeroSection - the Hero can  be full width

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
 import HeroSection from "@/sections/hero"
 
 const page = () => {
-  return (
-    <div className="flex justify-center p-10">
-        <HeroSection />
-    </div>
-  )
+  return <HeroSection />
 }
 
 export default page
