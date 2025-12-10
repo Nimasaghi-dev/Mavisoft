28c417fa659c753e248c4794fb51878f403c185d
nima <nimasaghi.dev@gmail.com>
2025-12-10 15:49:21 +0100

    Make the navbar complete transparent

--- src/components/top-navbar.tsx
+++ src/components/top-navbar.tsx
@@ -17,7 +17,7 @@ const menuItems = [
 
 export function TopNavbar() {
   return (
-    <header className="animate-fadeSlide fixed top-0 right-0 left-0 z-50 bg-zinc-950/80 backdrop-blur-md">
+    <header className="animate-fadeSlide fixed top-0 right-0 left-0 z-50 bg-transparent backdrop-blur-md">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <Navbar>
           {/* LEFT: Logo */}
