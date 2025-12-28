94cea89e1e7bfbd550048d3021cea1f402f23619
nima <nimasaghi.dev@gmail.com>
2025-12-28 13:49:01 +0100

    feat: add "interview with teddy" to press modal window

--- src/data/press-items.ts
+++ src/data/press-items.ts
@@ -239,4 +239,81 @@ export const pressItems: ArticleItem[] = [
       },
     ],
   },
+  {
+    id: 'Interview-with-Teddy',
+    title: `Interview with Theodor Preststulen, CEO of Mavisoft at AntwerpXL`,
+    date: '2022-Apr-26',
+    image: '/press/port-containers.jpg',
+    detailImage: '/press/teddy-meeting.jpg',
+    detailImageCaption: 'The Railway Pier in Sandridge, Melbourne State Library Victoria',
+    video: '',
+    excerpt: `We sat down with Theodor Preststulen, CEO of first-time AXL exhibitor Mavisoft – the world’s leading researcher of automated...`,
+    category: '',
+    content: [
+      {
+        h3: `Interview with Theodor Preststulen, CEO of Mavisoft at AntwerpXL`,
+        paragraphs: [
+          `We sat down with Theodor Preststulen, CEO of first-time AXL exhibitor Mavisoft – the world’s leading researcher of automated industrial surveying – to talk about the show, industry trends, and the impact of COVID-19 has had on business.`,
+        ],
+      },
+      {
+        h3: `What is it about Antwerp XL that made you want to exhibit for the first time in 2020?`,
+        paragraphs: [
+          `Antwerp XL has great attributes, particularly when it comes to location, network, and ambition; what I mean by this is that they are good at pushing innovation through partners such as PortXL, which for us is a must when presenting at a conference. Apart from that, they have a solid track record so we’re sure the work we are putting into this will pay off with a good level of exposure.`,
+        ],
+      },
+      {
+        h3: `What will you be showcasing at the innovation zone at Antwerp XL?`,
+        paragraphs: [
+          `At AntwerpXL, we will be displaying two of our products, Infrascan, and Scantainr. They are both focused on damage detection using our specialized AI algorithms. Infrascan is used for industrial surveying of any metal or concrete based assets, where we detect defects based on the source and severity of the damage, and this information can then be used for repair optimisation and budget management. Scantainr is a damage surveying solution specific to shipping containers, the largest value propositions lie in preventing damage claims, as well as repair and maintenance management. As a company we are currently focused very much on damage detection, however, we do plan on expanding our horizons in the coming years.`,
+        ],
+      },
+      {
+        h3: `What do you think will be the most prevalent industry trends and challenges in the next 12 months?`,
+        paragraphs: [
+          `Remote work and monitoring is the first thing that springs to mind. To give an example, if an asset needs to be surveyed, which today is usually done with drones, a live feed can be sent anywhere, giving surveyors the ability to analyse feeds remotely. The Rotterdam World Gateway is already doing this in the port where container discharging is almost fully automated, with autonomous trucks and cranes being controlled from offices. Once you combine this remote access with the support of AI, it allows people not only to work from home but also gives them more insight than they had before, resulting in less work needing to be done by humans.`,
+        ],
+      },
+      {
+        h3: `Which trends and challenges will impact your business most?`,
+        paragraphs: [
+          `Remote analytics is a huge trend. Surveying work has always required someone to be physically present, but now it can now be automated with the use of Artificial Intelligence. For tasks that are more complex, AI can act as an ‘assistant’ to help spot anomalies that humans can’t, in other words, prevent human error. It’s our bread and butter so it will impact us positively.`,
+        ],
+      },
+      {
+        h3: `What new industry innovation or technology has impressed you the most?`,
+        paragraphs: [
+          `What impresses us the most is how new companies are using AI to aid in all kinds of processes. From autonomous shipping to robots capable of fulfilling orders, the list never ends. It has untold applications; the possibilities are endless.`,
+        ],
+      },
+      {
+        h3: `What has been your organisation’s biggest accomplishment?`,
+        paragraphs: [
+          `Our biggest accomplishments during the quarantine period have been the productisation of our services into two offerings, Infrascan & Scantainr. We still have multiple services we plan on converting to products, however, these are the two most promising ones at the moment for us.`,
+        ],
+      },
+      {
+        h3: `What are the most important goals that your organisation has set over the next 5-10 years?`,
+        paragraphs: [
+          `Our goal in the next 5-10 years is to be a leading provider in automated damage detection for high-value problem-solving. This will be provided through uniquely built platforms that provide the ideal balance between performance and cost.`,
+        ],
+      },
+      {
+        image: '/press/teddy-antwerp-selection.jpg',
+        imageCaption: 'Mavisoft CEO Theodor Preststulen (right) getting ready to pitch at the 2019 Antwerp Selection Day',
+      },
+      {
+        h3: `What impact has COVID-19 had on your business?`,
+        paragraphs: [
+          `Apart from a handful of projects being put on hold, Mavisoft has managed to adapt fairly quickly to this new environment we are all faced with. The largest impact it has had on the company at this point is the lack of physical interaction between the team members. We take company culture seriously and the most challenging part about COVID is maintaining this interaction through online engagement which isn’t always as easy as you’d think. Apart from this, we think the larger impacts this will have on the market are yet to come, so we are doing everything we can to prepare ourselves.`,
+        ],
+      },
+      {
+        h3:'What do you think the long-term impact of COVID-19 will be on the industry?',
+        paragraphs: [
+          `Apart from the economic impact, I think COVID is going to really shine a light on flaws in the industry; first and foremost, the conception that one must be present at work in order to fulfil their obligations. Like all industries, I think we have a tendency to believe that working from home will lead to procrastination or difficulty to focus, simply because that’s exactly what you do when you get home, you relax, cook, and enjoy your hobbies. Since COVID is literally forcing people to work from home, we’re also forced to adapt, which before was rarely an option. If people get used to working from home and they like it, chances are they’re going to love it without the quarantine. This is something that I think companies are just going to have to accept. With that said, I think it’s well past time we prepare for remote controlling the port anyway, which is not only going to help centralise control but increase safety as well.`,
+        ],
+      },
+    ],
+  },
 ]
